ef65b21d7859e0bc857ec3d14c46c95e8de4159f
Rich Collier <rcollier@moguldom.com>
2016-03-30 22:39:51 -0400

    Allow additional types of avatars in autocomplete results - Use BuddyPress avatars if BuddyPress is active and default to Gravatar if not - Add coauthors_avatar_url filter to allow other plugins or themes to change the avatar URL for each author - Pass avatar size into localized Javascript to allow sizing as globally configured

--- co-authors-plus.php
+++ co-authors-plus.php
@@ -1068,8 +1068,14 @@ class CoAuthors_Plus {
 
 		// Loop through authors and store the necessary data in the response array
 		foreach ( $authors as $author ) {
+			// Fetch buddypress avatar if present
+			if ( function_exists( 'bp_core_fetch_avatar' ) ) {
+				$avatar = bp_core_fetch_avatar( array( 
+					'item_id' => $author->ID, 
+					'html' => false, 
+				));
 			// Get Gravatar URL if this is a guest author
-			if ( 'guest-author' == $author->type ) {
+			} elseif ( 'guest-author' == $author->type ) {
 				$hash = md5( $author->user_email );
 				$avatar = sprintf( 'https://www.gravatar.com/avatar/%s?s=%s', $hash, $this->gravatar_size );
 			// Normal users - get the local avatar URL
@@ -1431,6 +1437,7 @@ class CoAuthors_Plus {
 			'avatar_size' => absint( $this->gravatar_size ), 
 			'allow_add_guest_authors' => current_user_can( 'edit_users' ),
 			'nonce' => wp_create_nonce( 'coauthors' ),
+			'avatar_size' => absint( $this->gravatar_size ), 
 		);
 		
 		wp_localize_script( 'co-authors-plus-js', 'coAuthorsPlusStrings', $js_strings );
